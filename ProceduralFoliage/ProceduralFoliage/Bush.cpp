61c7d7f3c3e098fe829b0be927dc99e8bc5a77e3
Unknown <Elliot2.martin@live.uwe.ac.uk>
2018-11-18 21:06:23 +0000

    Added Length, radius, need to add rules to generate bush

--- ProceduralFoliage/ProceduralFoliage/Bush.cpp
+++ ProceduralFoliage/ProceduralFoliage/Bush.cpp
@@ -1,5 +1,5 @@
 #include "Bush.h"
-
+#include <time.h>
 Bush::Bush()
 {
 
@@ -12,16 +12,28 @@ Bush::~Bush()
 
 void Bush::Generate(ID3D11Device* device, ID3D11DeviceContext* device_context)
 {
+	srand(time(NULL));
 	L_system = new LSystem;
 	L_system->GenerateSystem();
-
-	std::string test = "+[+LFXYF[]+LFXXZF[";
+	float r;
+	std::string test = "+]LRFXF[+LRFXYF[+]LRFXYZF[+]]LRFYZF[";
 
 	XMFLOAT3 pos = { 0, 0, 0 };
 	XMFLOAT4 rot = { 0, 0, 0, 1 };
 	XMFLOAT3 scale = { 0.1, 1, 0.1 };
-	int model_count = -1;
-	int selected = -1;
+	int selected = 1;
+	Model* previous_model;
+
+	current_model = new Model;
+	current_model->Init(device, device_context, (char*)"../Engine/Models/Cube.txt", (char*)"../Engine/Textures/Green.tga");
+	model_list.push_back(current_model);
+	if (current_model)
+	{
+		current_model->SetPosition(pos.x, pos.y, pos.z);
+		current_model->SetRotation(rot.x, rot.y, rot.z, 1.0f);
+		current_model->SetScale(scale.x, scale.y, scale.z);
+	}
+
 	for (int iter = 0; iter < 1; iter++)
 	{
 		for (int i = 0; i < test.size(); i++)
@@ -34,17 +46,17 @@ void Bush::Generate(ID3D11Device* device, ID3D11DeviceContext* device_context)
 				current_model->Init(device, device_context, (char*)"../Engine/Models/Cube.txt", (char*)"../Engine/Textures/Green.tga");
 			}
 				break;
-			case '-':
-				
-				break;
 			case 'X':
-				rot.x += 20;
+				r = rand() % 360 + 10;
+				rot.x += r;
 				break;
 			case 'Y':
-				rot.y += 20;
+				r = rand() % 360 + 10;
+				rot.y += r;
 				break;
 			case 'Z':
-				rot.z += 20;
+				r = rand() % 360 + 10;
+				rot.z += r;
 				break;
 			case 'F':
 				current_model->MoveUP(1);
@@ -52,21 +64,21 @@ void Bush::Generate(ID3D11Device* device, ID3D11DeviceContext* device_context)
 				break;
 			case '[':
 				model_list.push_back(current_model);
-				model_count++;
 				selected++;
 				break;
 			case ']':
 				selected--;
-				current_model = model_list[selected];
-				XMStoreFloat3(&pos, current_model->GetPosition());
-				XMStoreFloat4(&rot, current_model->GetRotation());
-				XMStoreFloat3(&scale, current_model->GetScale());
+				previous_model = model_list[selected];
+				XMStoreFloat3(&pos, previous_model->GetPosition());
+				XMStoreFloat4(&rot, previous_model->GetRotation());
+				XMStoreFloat3(&scale, previous_model->GetScale());
 				break;
 			case 'R':
-				//scale radius down
+				scale.x /= 1.5f;
+				scale.z /= 1.5f;
 				break;
 			case 'L':
-				scale.y -= 0.25f;
+				scale.y /= 1.5f;
 				break;
 			}
 			if (current_model)
