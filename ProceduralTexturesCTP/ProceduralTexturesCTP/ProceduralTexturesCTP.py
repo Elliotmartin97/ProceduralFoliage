587b4cea2d97c7e3e752a3bbde77c23ea4f1839a
Elliot Martin <Elliot2.martin@live.uwe.ac.uk>
2019-02-11 13:08:23 +0000

    Conditional GAN

--- ProceduralTexturesCTP/ProceduralTexturesCTP/ProceduralTexturesCTP.py
+++ ProceduralTexturesCTP/ProceduralTexturesCTP/ProceduralTexturesCTP.py
@@ -7,51 +7,66 @@ import matplotlib.gridspec as gridspec
 import os
 from tensorflow.examples.tutorials.mnist import input_data
 
-mnist=input_data.read_data_sets("MNIST_data")
+mnist = input_data.read_data_sets('../../MNIST_data', one_hot=True)
+mb_size = 64
+Z_dim = 100
+X_dim = mnist.train.images.shape[1]
+y_dim = mnist.train.labels.shape[1]
+h_dim = 128
+
 
 def xavier_init(size):
     in_dim = size[0]
     xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
     return tf.random_normal(shape=size, stddev=xavier_stddev)
 
+
+""" Discriminator Net model """
 X = tf.placeholder(tf.float32, shape=[None, 784])
+y = tf.placeholder(tf.float32, shape=[None, y_dim])
 
-D_W1 = tf.Variable(xavier_init([784, 128]))
-D_b1 = tf.Variable(tf.zeros(shape=[128]))
+D_W1 = tf.Variable(xavier_init([X_dim + y_dim, h_dim]))
+D_b1 = tf.Variable(tf.zeros(shape=[h_dim]))
 
-D_W2 = tf.Variable(xavier_init([128, 1]))
+D_W2 = tf.Variable(xavier_init([h_dim, 1]))
 D_b2 = tf.Variable(tf.zeros(shape=[1]))
 
 theta_D = [D_W1, D_W2, D_b1, D_b2]
 
 
-Z = tf.placeholder(tf.float32, shape=[None, 100])
+def discriminator(x, y):
+    inputs = tf.concat(axis=1, values=[x, y])
+    D_h1 = tf.nn.relu(tf.matmul(inputs, D_W1) + D_b1)
+    D_logit = tf.matmul(D_h1, D_W2) + D_b2
+    D_prob = tf.nn.sigmoid(D_logit)
 
-G_W1 = tf.Variable(xavier_init([100, 128]))
-G_b1 = tf.Variable(tf.zeros(shape=[128]))
+    return D_prob, D_logit
 
-G_W2 = tf.Variable(xavier_init([128, 784]))
-G_b2 = tf.Variable(tf.zeros(shape=[784]))
+
+""" Generator Net model """
+Z = tf.placeholder(tf.float32, shape=[None, Z_dim])
+
+G_W1 = tf.Variable(xavier_init([Z_dim + y_dim, h_dim]))
+G_b1 = tf.Variable(tf.zeros(shape=[h_dim]))
+
+G_W2 = tf.Variable(xavier_init([h_dim, X_dim]))
+G_b2 = tf.Variable(tf.zeros(shape=[X_dim]))
 
 theta_G = [G_W1, G_W2, G_b1, G_b2]
 
-def sample_Z(m, n):
-    return np.random.uniform(-1., 1., size=[m, n])
 
-def generator(z):
-    G_h1 = tf.nn.relu(tf.matmul(z, G_W1) + G_b1)
+def generator(z, y):
+    inputs = tf.concat(axis=1, values=[z, y])
+    G_h1 = tf.nn.relu(tf.matmul(inputs, G_W1) + G_b1)
     G_log_prob = tf.matmul(G_h1, G_W2) + G_b2
     G_prob = tf.nn.sigmoid(G_log_prob)
 
     return G_prob
 
 
-def discriminator(x):
-    D_h1 = tf.nn.relu(tf.matmul(x, D_W1) + D_b1)
-    D_logit = tf.matmul(D_h1, D_W2) + D_b2
-    D_prob = tf.nn.sigmoid(D_logit)
+def sample_Z(m, n):
+    return np.random.uniform(-1., 1., size=[m, n])
 
-    return D_prob, D_logit
 
 def plot(samples):
     fig = plt.figure(figsize=(4, 4))
@@ -69,23 +84,18 @@ def plot(samples):
     return fig
 
 
-G_sample = generator(Z)
-D_real, D_logit_real = discriminator(X)
-D_fake, D_logit_fake = discriminator(G_sample)
+G_sample = generator(Z, y)
+D_real, D_logit_real = discriminator(X, y)
+D_fake, D_logit_fake = discriminator(G_sample, y)
 
 D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_real, labels=tf.ones_like(D_logit_real)))
 D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_fake, labels=tf.zeros_like(D_logit_fake)))
-
 D_loss = D_loss_real + D_loss_fake
 G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_fake, labels=tf.ones_like(D_logit_fake)))
 
 D_solver = tf.train.AdamOptimizer().minimize(D_loss, var_list=theta_D)
 G_solver = tf.train.AdamOptimizer().minimize(G_loss, var_list=theta_G)
 
-mb_size = 128
-Z_dim = 100
-
-mnist = input_data.read_data_sets('../../MNIST_data', one_hot=True)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
@@ -97,17 +107,24 @@ i = 0
 
 for it in range(1000000):
     if it % 1000 == 0:
-        samples = sess.run(G_sample, feed_dict={Z: sample_Z(1, Z_dim)})
+        n_sample = 16
+
+        Z_sample = sample_Z(n_sample, Z_dim)
+        y_sample = np.zeros(shape=[n_sample, y_dim])
+        y_sample[:, 7] = 1
+
+        samples = sess.run(G_sample, feed_dict={Z: Z_sample, y:y_sample})
 
         fig = plot(samples)
-        plt.savefig('out/MNIST/Random/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
+        plt.savefig('out/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
         i += 1
         plt.close(fig)
 
-    X_mb, _ = mnist.train.next_batch(mb_size)
+    X_mb, y_mb = mnist.train.next_batch(mb_size)
 
-    _, D_loss_curr = sess.run([D_solver, D_loss], feed_dict={X: X_mb, Z: sample_Z(mb_size, Z_dim)})
-    _, G_loss_curr = sess.run([G_solver, G_loss], feed_dict={Z: sample_Z(mb_size, Z_dim)})
+    Z_sample = sample_Z(mb_size, Z_dim)
+    _, D_loss_curr = sess.run([D_solver, D_loss], feed_dict={X: X_mb, Z: Z_sample, y:y_mb})
+    _, G_loss_curr = sess.run([G_solver, G_loss], feed_dict={Z: Z_sample, y:y_mb})
 
     if it % 1000 == 0:
         print('Iter: {}'.format(it))
